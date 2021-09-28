133be659617733270d94d0d10048748a949cff8d
Jorge Teixeira <jorgemrt@protonmail.com>
2021-09-28 17:12:49 +0100

    API with NestJS #2. Setting up a PostgreSQL database with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
+import Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [],
   providers: [],
 })
